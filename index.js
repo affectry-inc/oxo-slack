8eb1e2db737d5210e281026bbeea0f039adc9ade
xiz <shizuka.kakimoto@linkage.jp.net>
2017-03-13 01:05:40 +0900

    dotenv ignore env

--- index.js
+++ index.js
@@ -14,7 +14,7 @@
   After you've added the app, try talking to your bot!
 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
 
-if (process.env.NODE_ENV !== 'production') require('dotenv').config();
+require('dotenv').config();
 
 var Botkit = require('botkit');
 var MongoUrl = process.env.MONGODB_URI || 'mongodb://localhost:27017/oxo';
